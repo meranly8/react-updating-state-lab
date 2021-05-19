51df38b28cd970ae5ddf593d6f7fd2e4421d9645
Michelle Ranly <meranly8@gmail.com>
2021-05-18 22:20:51 -0500

    commit tree 80b85a7ea75f74b9c3a7cf183bc2802667fcd936

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+    state = {
+        timesClicked: 0
+    }
+    
+    handleIncrement = () => {
+        this.setState(prevState => {
+            return {
+                timesClicked: prevState.timesClicked += 1
+            }
+        })
+    }
+
+    render() {
+        return (
+            <label>Times Clicked  <button onClick={this.handleIncrement}>{this.state.timesClicked}</button></label>
+        )
+    }
+}
